ed264c7f7e86046603ba3a4fb7f519d2945c9c7d
Jonathan Pope <jonathanmpope@gmail.com>
2022-09-24 09:33:46 -0600

    test: add sad path test for passwords not matching

--- spec/requests/user_request_spec.rb
+++ spec/requests/user_request_spec.rb
@@ -29,4 +29,26 @@ describe 'User API' do
         expect(info[:attributes]).to have_key(:api_key)
         expect(info[:attributes][:api_key]).to be_a String 
     end 
+
+    it 'returns an error if passwords do not match' do
+        body = {
+                "email": "whatever12345@example.com",
+                "password": "password",
+                "password_confirmation": "password1"
+                }
+
+        post "/api/v1/users", params: body 
+
+        expect(response).to_not be_successful
+
+        expect(response.status).to eq(422)
+    
+        info = JSON.parse(response.body, symbolize_names: true)
+       
+        expect(info).to have_key(:error)
+        expect(info[:error]).to be_a Hash 
+
+        expect(info[:error]).to have_key(:password_confirmation)
+        expect(info[:error][:password_confirmation]).to eq(["doesn't match Password"])
+    end 
 end 
