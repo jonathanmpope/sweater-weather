3ba88c9c11c9a51c46ab5414a19220c9319091f4
Jonathan Pope <jonathanmpope@gmail.com>
2022-09-26 11:52:35 -0600

    feat: add sad path for quanitiy that is a non-integer passed in as argument

--- app/facades/booksearch_facade.rb
+++ app/facades/booksearch_facade.rb
@@ -1,11 +1,14 @@
 class BooksearchFacade
 
-    def self.get_info(location, quantity)
-        json = BooksearchService.get_info(location)
-         
-        book_hash = {}
-        book_hash[:total_books] = json[:numFound]
-        book_hash[:books] = json[:docs][0..(quantity.to_i - 1)].map { |d| Books.new(d) }
-        book_hash 
+    def self.get_info(location, quantity) 
+        if quantity.to_i.class == Integer && quantity.to_i > 0
+            json = BooksearchService.get_info(location) 
+            book_hash = {}
+            book_hash[:total_books] = json[:numFound]
+            book_hash[:books] = json[:docs][0..(quantity.to_i - 1)].map { |d| Books.new(d) }
+            book_hash
+        else 
+            "You must submit an integer like 5"
+        end 
     end 
 end 
--- spec/facades/book_search_facade_spec.rb
+++ spec/facades/book_search_facade_spec.rb
@@ -11,4 +11,15 @@ RSpec.describe BooksearchFacade do
             expect(results[:books]).to be_all Books
         end 
     end
+
+    describe "#get_info - sadpath" do
+        it 'returns book info', :vcr do 
+            results = BooksearchFacade.get_info("denver,CO", "five")
+
+            expect(results).to be_a String 
+            expect(results).to eq "You must submit an integer like 5"
+        end 
+    end
 end 
+
+        
